c6019a15b094cf1ba1777ea7931f418aaf292822
robinsse <srobins@byupathway.edu>
2025-11-04 19:32:02 -0700

    W02 Learning Activity - Prophets

---
+++ scripts/prophets.js
@@ -0,0 +1,34 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+getProphetData();
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullname = document.createElement('h2');
+        let birthday = document.createElement('p');
+        let birthplace = document.createElement('p');
+        let portrait = document.createElement('img');
+
+        fullname.textContent = `${prophet.name} ${prophet.lastname}`;
+        birthday.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullname);
+        card.appendChild(birthday);
+        card.appendChild(birthplace);
+        card.appendChild(portrait);
+        cards.appendChild(card);
+    });
+}
