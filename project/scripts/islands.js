103bbc6ee11ba584a5975da9f24927055abf704c
robinsse <srobins@byupathway.edu>
2025-12-04 13:52:02 -0700

    Home Draft

---
+++ project/scripts/islands.js
@@ -0,0 +1,25 @@
+const url = 'https://robinsse.github.io/wdd231/chamber/data/islands.json';
+const islandsContainer = document.querySelector('#islands');
+
+const displayIslands = (islandsData) => {
+    islandsData.forEach((item) => {
+        let island = document.createElement('section');
+        let title = document.createElement('h3');
+        let description = document.createElement('p');
+
+        title.innerHTML = `<strong>${item.title}</strong>`;
+        description.textContent = item.description;
+
+        island.appendChild(title);
+        island.appendChild(description);
+
+        islandsContainer.appendChild(island);
+    });
+}
+
+async function getIslandData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayIslands(data.islands);
+}
+getIslandData();
--- project/scripts/thankyou.js
+++ project/scripts/thankyou.js
@@ -6,19 +6,20 @@ console.log(memberInfo);
 
 console.log(memberInfo.get('first'));
 console.log(memberInfo.get('last'));
-console.log(memberInfo.get('org-title'));
 console.log(memberInfo.get('email'));
 console.log(memberInfo.get('phone'));
-console.log(memberInfo.get('business'));
-console.log(memberInfo.get('membership'));
-console.log(memberInfo.get('description'));
+console.log(memberInfo.get('islands'));
+console.log(memberInfo.get('travelers'));
+console.log(memberInfo.get('stars'));
+console.log(memberInfo.get('activities'));
 console.log(memberInfo.get('timestamp'));
 
 document.querySelector('#results').innerHTML = `
-<h3>Member Info</h3>
+<h3>Traveler Info</h3>
 <p>${memberInfo.get('first')} ${memberInfo.get('last')}</p>
-<p>${memberInfo.get('org-title')} @ ${memberInfo.get('business')} (${memberInfo.get('description')})</p>
 <p>Email: ${memberInfo.get('email')}</p>
 <p>Phone: ${memberInfo.get('phone')}</p>
-<p>Signed up for ${memberInfo.get('membership')} membership on ${memberInfo.get('timestamp')}</p>
+<p>Wants to visit ${memberInfo.get('islands')} with ${memberInfo.get('travelers')} travelers and stay at a ${memberInfo.get('stars')} star hotel.</p>
+<p>Desired activities: ${memberInfo.get('activities')}</p>
+<p>Submitted "Book with a Travel Agent" form on ${memberInfo.get('timestamp')}</p>
 `
